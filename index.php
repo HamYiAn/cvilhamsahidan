c6f103a1e3001856b83a6d56b5db93e8caa08631
HamYiAn <ilamzblues@gmail.com>
2021-12-10 16:23:15 +0700

    some init msg

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Curiculum Vitae</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin"/>
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap" media="print" onload="this.media='all'"/>
+    <noscript>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&amp;family=Roboto:wght@300;400;500;700&amp;display=swap"/>
+    </noscript>
+    <link href="css/font-awesome/css/all.min.css?ver=1.2.0" rel="stylesheet">
+    <link href="css/bootstrap.min.css?ver=1.2.0" rel="stylesheet">
+    <link href="css/aos.css?ver=1.2.0" rel="stylesheet">
+    <link href="css/main.css?ver=1.2.0" rel="stylesheet">
+    <noscript>
+      <style type="text/css">
+        [data-aos] {
+            opacity: 1 !important;
+            transform: translate(0) scale(1) !important;
+        }
+      </style>
+    </noscript>
+  </head>
+  <body id="top">
+    <header class="d-print-none">
+      <div class="container text-center text-lg-left">
+        <div class="py-3 clearfix">
+          <h1 class="site-title mb-0">CURICULLUM VITAE</h1>
+          <div class="site-nav">
+            <nav role="navigation">
+              <ul class="nav justify-content-center">
+                <li class="nav-item"><a class="nav-link" href="https://twitter.com/Minji82946218" title="Twitter"><i class="fab fa-twitter"></i><span class="menu-title sr-only">Twitter</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="https://web.facebook.com/ilham.poi.1232" title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="https://www.instagram.com/ham_yi_an/" title="Instagram"><i class="fab fa-instagram"></i><span class="menu-title sr-only">Instagram</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="https://wa.me/+6283101204259" title="Whatsapp"><i class="fab fa-whatsapp"></i><span class="menu-title sr-only">Github</span></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="page-content">
+      <div class="container">
+<div class="cover shadow-lg bg-white">
+  <div class="cover-bg p-3 p-lg-4 text-white">
+    <div class="row">
+      <div class="col-lg-4 col-md-5">
+        <div class="avatar hover-effect bg-white shadow-sm p-1"><img src="images/avatar.jpg" width="200" height="200"/></div>
+      </div>
+      <div class="col-lg-8 col-md-7 text-center text-md-start">
+        <h2 class="h1 mt-2" data-aos="fade-left" data-aos-delay="0">Ilham Sahidan</h2>
+        <p data-aos="fade-left" data-aos-delay="100">Youtuber & Web Developer</p>
+        <div class="d-print-none" data-aos="fade-left" data-aos-delay="200"><a class="btn btn-light text-dark shadow-sm mt-1 me-1" href="hasil.pdf" target="_blank">Download CV</a><a class="btn btn-success shadow-sm mt-1" href="#contact">Hubungi Saya</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="about-section pt-4 px-3 px-lg-4 mt-1">
+    <div class="row">
+      <div class="col-md-6">
+        <h2 class="h3 mb-3">Tentang Saya</h2>
+        <p>
+          Hallo saya Ilham Sahidan, disini saya memiliki keterampilan yang cukup bagus dalam pemograman, baik dalam front-end maupun back-end.
+          Profesional TI yang berorientasi detail dengan pengalaman 1 tahun sebagai software support specialist dan system/network technician. Terampil dalam mengoperasikan berbagai platform, memiliki kemampuan komunikasi lisan dan verbal yang baik, dan mampu menjelaskan masalah software yang kompleks dalam istilah yang mudah dipahami.
+          
+        </p>
+      </div>
+      <div class="col-md-5 offset-md-1">
+        <div class="row mt-2">
+          <div class="col-sm-4">
+            <div class="pb-1">Umur</div>
+          </div>
+          <div class="col-sm-8">
+            <div class="pb-1 text-secondary">18</div>
+          </div>
+          <div class="col-sm-4">
+            <div class="pb-1">Email</div>
+          </div>
+          <div class="col-sm-8">
+            <div class="pb-1 text-secondary">21110372@stmik-mi.ac.id</div>
+          </div>
+          <div class="col-sm-4">
+            <div class="pb-1">No. Hp</div>
+          </div>
+          <div class="col-sm-8">
+            <div class="pb-1 text-secondary">+62 831-0120-4259</div>
+          </div>
+          <div class="col-sm-4">
+            <div class="pb-1">Alamat</div>
+          </div>
+          <div class="col-sm-8">
+            <div class="pb-1 text-secondary">Lembang, Bandung Barat </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr class="d-print-none"/>
+  <div class="skills-section px-3 px-lg-4">
+    <h2 class="h3 mb-3">Professional Skills</h2>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="mb-2"><span>HTML</span>
+          <div class="progress my-1">
+            <div class="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="100" data-aos-anchor=".skills-section" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+        <div class="mb-2"><span>CSS</span>
+          <div class="progress my-1">
+            <div class="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="200" data-aos-anchor=".skills-section" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+        <div class="mb-2"><span>JavaScript</span>
+          <div class="progress my-1">
+            <div class="progress-bar bg-primary" role="progressbar" data-aos="zoom-in-right" data-aos-delay="300" data-aos-anchor=".skills-section" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="mb-2"><span>Adobe Photoshop</span>
+          <div class="progress my-1">
+            <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right" data-aos-delay="400" data-aos-anchor=".skills-section" style="width: 80%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+        <div class="mb-2"><span>Adobe Priemer</span>
+          <div class="progress my-1">
+            <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right" data-aos-delay="500" data-aos-anchor=".skills-section" style="width: 85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+        <div class="mb-2"><span>Adobe After Effects</span>
+          <div class="progress my-1">
+            <div class="progress-bar bg-success" role="progressbar" data-aos="zoom-in-right" data-aos-delay="600" data-aos-anchor=".skills-section" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr class="d-print-none"/>
+  <div class="work-experience-section px-3 px-lg-4">
+    <h2 class="h3 mb-4">Pengalaman Kerja</h2>
+    <div class="timeline">
+      <div class="timeline-card timeline-card-primary card shadow-sm">
+        <div class="card-body">
+          <div class="h5 mb-1">Frontend Developer <span class="text-muted h6">at Universitas Pendidikan Indonesia </span></div>
+          <div class="text-muted text-small mb-2">Desember, 2019 – Maret, 2020 </div>
+          <div>Pada tahun 2019, saya dan Teman saya melaksankan PKL Di Universitas Pendikdikan Indonesia. Saya dan teman saya ditempatkan di LPPM UPI tepatnya dibagian Pemograman Front-end. 
+ 
+          </div>
+        </div>
+      </div>
+      <div class="timeline-card timeline-card-primary card shadow-sm">
+        <div class="card-body">
+          <div class="h5 mb-1">Web Designer <span class="text-muted h6">at Home</span></div>
+          <div class="text-muted text-small mb-2">Agustus, 2020 - Mei, 2021 </div>
+          <div>Saya dan teman saya mencoba mencari pekerjaan secara online. Dikarenakan adanya covid-19 saya dan teman teman saya mencoba untuk membuat sebuah proyek web di rumah dengan bagiannya masing masing, dan saya terpilih untuk web desain. </div>
+        </div>
+      </div>
+      <div class="timeline-card timeline-card-primary card shadow-sm">
+        <div class="card-body">
+          <div class="h5 mb-1">Kimchi Food <span class="text-muted h6">at Shopee Food</span></div>
+          <div class="text-muted text-small mb-2">Oktober, 2021 – November 2021</div>
+          <div>Pada akhir oktober saya dan teman teman saya mencoba untuk membuka peluang Bisnis, namun kami tutup karena kurang dana sama kurang pengalaman dalam bisnis. </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr class="d-print-none"/>
+  <div class="page-break"></div>
+  <div class="education-section px-3 px-lg-4 pb-4">
+    <h2 class="h3 mb-4">Education</h2>
+    <div class="timeline">
+      <div class="timeline-card timeline-card-success card shadow-sm">
+        <div class="card-body">
+          <div class="h5 mb-1">Sekolah Dasar<span class="text-muted h6"> from SD Negeri 2 Cibodas</span></div>
+          <div class="text-muted text-small mb-2">2009 - 2015</div>
+          <div>Pada tahun 2009 saya masuk SD Negeri 2 Cibodas, Saya hanya juara pada kelas 4 dengan juara ke 2 dari 32 murid, saya lulus pada tahun 2015 tentu saja Bersama teman teman saya. 
+ 
+          </div>
+        </div>
+      </div>
+      <div class="timeline-card timeline-card-success card shadow-sm">
+        <div class="card-body">
+          <div class="h5 mb-1">Sekolah Menengah Pertama <span class="text-muted h6"> from SMP Negeri 4 Lembang </span></div>
+          <div class="text-muted text-small mb-2">2015 - 2018</div>
+          <div>Saya Melanjutkan sekolah di SMP Negeri 4 Lembang Bersama Teman Teman SD Saya. Dari kelas 7 sampai kelas 9 saya hanya juara 1 kali yaitu juara pertama di kelas 8. Dan saya lulus pada tahun 2018. 
+ 
+          </div>
+        </div>
+      </div>
+      <div class="timeline-card timeline-card-success card shadow-sm">
+        <div class="card-body">
+          <div class="h5 mb-1">Sekolah Menengah Kejuruan <span class="text-muted h6">from SMK Bina Wisata </span></div>
+          <div class="text-muted text-small mb-2">2018 - 2021</div>
+          <div>Saya melanjutkan sekolah di SMK Bina Wisata Lembang dengan tujuan agar bias belajar pemograman karena saya masuk SMK Bina Wisata Lembang masuk Jurusan RPL. Di SMK Saya tidak juara hanya masuk rangking 7 dari 52 Murid.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr class="d-print-none"/>
+  <div class="contant-section px-3 px-lg-4 pb-4" id="contact">
+    <h2 class="h3 text mb-3">Kontak</h2>
+    <div class="row">
+      <div class="col-md-7 d-print-none">
+        <div class="my-2"><form action="https://formspree.io/f/xyyoyewq"
+    method="POST">
+  <div class="row">
+    <div class="col-6">
+      <input class="form-control" type="text" id="name" name="name" placeholder="Nama Anda" required>
+    </div>
+    <div class="col-6">
+      <input class="form-control" type="email" id="email" name="_replyto" placeholder="E-mail Anda" required>
+    </div>
+  </div>
+  <div class="form-group my-2">
+    <textarea class="form-control" style="resize: none;" id="message" name="message" rows="4"  placeholder="Pesan Anda" required></textarea>
+  </div>
+  <button class="btn btn-primary mt-2" type="submit">Kirim</button>
+</form>
+        </div>
+      </div>
+      <div class="col">
+        <div class="mt-2">
+          <h3 class="h6">Alamat</h3>
+          <div class="pb-2 text-secondary">Cibodas, Lembang</div>
+          <h3 class="h6">No. HP</h3>
+          <div class="pb-2 text-secondary">+62 815-7316-1801</div>
+          <h3 class="h6">E-mail</h3>
+          <div class="pb-2 text-secondary">ilamzblues@gmail.com</div>
+        </div>
+      </div>
+      <div class="col d-none d-print-block">
+        <div class="mt-2">
+          <div>
+            <div class="mb-2">
+              <div class="text-dark"><i class="fab fa-twitter mr-1"></i><span>https://twitter.com/Minji82946218</span>
+              </div>
+            </div>
+            <div class="mb-2">
+              <div class="text-dark"><i class="fab fa-facebook mr-1"></i><span>https://web.facebook.com/ilham.poi.1232</span>
+              </div>
+            </div>
+            <div class="mb-2">
+              <div class="text-dark"><i class="fab fa-instagram mr-1"></i><span>https://www.instagram.com/ham_yi_an/</span>
+              </div>
+            </div>
+            <div class="mb-2">
+              <div class="text-dark"><i class="fab fa-whatsapp mr-1"></i><span>https://wa.me/+6283101204259</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div></div>
+    </div>
+    <footer class="pt-4 pb-4 text-muted text-center d-print-none">
+      <div class="container">
+        <div class="my-3">
+          <div class="h4">Ilham Sahidan</div>
+          <div class="footer-nav">
+            <nav role="navigation">
+              <ul class="nav justify-content-center">
+                <li class="nav-item"><a class="nav-link" href="https://twitter.com/Minji82946218" title="Twitter"><i class="fab fa-twitter"></i><span class="menu-title sr-only">Twitter</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="https://web.facebook.com/ilham.poi.1232" title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="https://www.instagram.com/ham_yi_an/" title="Instagram"><i class="fab fa-instagram"></i><span class="menu-title sr-only">Instagram</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="https://wa.me/+6283101204259" title="Whatsapp"><i class="fab fa-whatsapp"></i><span class="menu-title sr-only">Github</span></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="text-small">
+          <div class="mb-1">&copy; Ilham Sahidan 21110372.</div>
+          <div>2021</a></div>
+        </div>
+      </div>
+    </footer>
+    <script src="scripts/bootstrap.bundle.min.js?ver=1.2.0"></script>
+    <script src="scripts/aos.js?ver=1.2.0"></script>
+    <script src="scripts/main.js?ver=1.2.0"></script>
+  </body>
+</html>
